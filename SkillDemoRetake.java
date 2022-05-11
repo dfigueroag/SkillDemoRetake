1726647fb1bdf51c033608f3d359b72f0ccced1b
dfigueroag <dfigueroaguerrero@gmail.com>
2022-05-10 23:26:48 -0700

    Added Test Files

--- SkillDemoRetake.java
+++ SkillDemoRetake.java
@@ -1,3 +1,5 @@
 public class SkillDemoRetake{
-
+    public static int square(int a){
+        return a*a;
+    }
 }
---
+++ SkillDemoRetakeTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoRetakeTester {
+    @Test
+    public void addition() {
+        assertEquals(8, SkillDemoRetake.square(3));
+    }
+}
